2c00860200c73e41e14bf073c47affc3d35e9d62
Mitchell Vitrano <mitch.vitrano@pff.com>
2024-09-26 21:22:52 -0700

    Initial Commit

---
+++ Taste/TasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasteApp.swift
+//  Taste
+//
+//  Created by Mitch Vitrano on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
